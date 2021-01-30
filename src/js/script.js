783f5d6c026cbff75a499dcee13fb533760e4665
DGMary <mblnkinakk@gmail.com>
2021-01-30 22:40:46 +0200

    Users list api was added.

--- src/js/script.js
+++ src/js/script.js
@@ -115,3 +115,41 @@ function initCountdown() {
     });
   });
 }
+
+getInfo();
+function getInfo() {
+  const request = new Request('https://squad-runners.in.ua/api/users/all');
+  const myList = document.querySelector('.user-list');
+  myList.classList.add('loader');
+  fetch(request)
+  .then(response => {
+    
+    if (response.status === 200) {      
+      return response.json();      
+    } else {
+      throw new Error('Что-то пошло не так на API сервере.');
+    }
+  })
+  .then(response => {
+    myList.classList.remove('loader');
+    for (var i = 0; i < 10; i++) {
+      var listItem = document.createElement('li');
+      listItem.classList.add('user-item');
+      listItem.innerHTML = `<a class="user-link" href="/users/642/info">
+                            <div class="user-info">
+                              <span class="user-index">${i+1}</span>
+                              <div class="user-img">
+                                <img src="${response[i].avatar}">
+                              </div>
+                              <span>${response[i].fullname}</span>
+                              <span class="user-team">${response[i].teamName}</span>
+                            </div>
+                            <span class="user-distance">${response[i].kilometersThisMonth}<span>км</span></span>
+                          </a>`
+      myList.appendChild(listItem);
+    }
+  }).catch(error => {
+    console.error(error);
+  });
+}
+  
